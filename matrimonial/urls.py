70b6b4d9504c7206455f7f1947b6bd7bc9f08037
Siddanta <35145840+Siddanta@users.noreply.github.com>
2019-12-10 13:23:38 +0545

    Add files via upload

---
+++ matrimonial/urls.py
@@ -0,0 +1,37 @@
+"""matrimonial URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
+from home.views import index
+from django.urls import path
+
+urlpatterns = [
+    url('^admin/', admin.site.urls),
+    url('^home/', include('home.urls')),
+    url('^$', index),
+
+    url('^accounts/', include('accounts.urls', namespace="accounts")),
+    url('^questions/', include('questions.urls')),
+
+    url('^like',include('likes.urls', namespace="likes")),
+
+    #url('^likes/')
+
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
